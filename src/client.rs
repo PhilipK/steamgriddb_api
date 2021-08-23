6b98d2087c230435f3ca5c1228c3efbfb66dc450
Philip Kristoffersen <philipkristoffersen@gmail.com>
2021-08-23 17:56:30 +0200

    Add search to client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use serde::{de::DeserializeOwned, Deserialize};
+
 use crate::{
     images::{
         get_images_by_game_id_url, get_images_by_game_ids_url, Image,
@@ -5,6 +7,7 @@ use crate::{
     },
     query_parameters::QueryType,
     response::{response_to_result, response_to_result_flat, SteamGridDbResult},
+    search::{get_search_url, InnerSearchResult, SearchResult},
 };
 
 pub struct Client {
@@ -40,37 +43,51 @@ impl Client {
         self.auth_key = auth_key;
     }
 
-    pub async fn fetch_images_for_id<'a>(
+    pub async fn get_images_for_id<'a>(
         &self,
         game_id: &str,
         config: &QueryType<'a>,
     ) -> Result<SteamGridDbResult<Vec<Image>>, Box<dyn std::error::Error>> {
         let url = get_images_by_game_id_url(self.base_url.as_str(), game_id, config);
-        let inner_response = self
-            .client
-            .get(url)
-            .bearer_auth(self.auth_key.as_str())
-            .send()
-            .await?
-            .json::<InnerImagesSingleIdResponse>()
+        let resposse = self
+            .make_request::<InnerImagesSingleIdResponse>(url.as_str())
             .await?;
-        Ok(response_to_result(inner_response))
+        Ok(response_to_result(resposse))
     }
 
-    pub async fn fetch_images_for_ids_url<'a>(
+    pub async fn get_images_for_ids<'a>(
         &self,
         game_id: &[&str],
         config: &QueryType<'a>,
     ) -> Result<SteamGridDbResult<Vec<SteamGridDbResult<Image>>>, Box<dyn std::error::Error>> {
         let url = get_images_by_game_ids_url(self.base_url.as_str(), game_id, config);
-        let inner_response = self
+
+        let resposse = self
+            .make_request::<InnerImagesMultipleIdsResponse>(url.as_str())
+            .await?;
+        Ok(response_to_result_flat(resposse))
+    }
+
+    pub async fn search(
+        &self,
+        query: &str,
+    ) -> Result<SteamGridDbResult<Vec<SearchResult>>, Box<dyn std::error::Error>> {
+        let url = get_search_url(self.base_url.as_str(), query);
+        let resposne = self.make_request::<InnerSearchResult>(url.as_str()).await?;
+        Ok(response_to_result(resposne))
+    }
+
+    async fn make_request<'de, T>(&self, url: &str) -> Result<T, Box<dyn std::error::Error>>
+    where
+        T: DeserializeOwned,
+    {
+        Ok(self
             .client
             .get(url)
             .bearer_auth(self.auth_key.as_str())
             .send()
             .await?
-            .json::<InnerImagesMultipleIdsResponse>()
-            .await?;
-        Ok(response_to_result_flat(inner_response))
+            .json::<T>()
+            .await?)
     }
 }
--- src/search.rs
+++ src/search.rs
@@ -7,7 +7,7 @@ pub fn get_search_url(base_url: &str, qeury: &str) -> String {
 }
 
 
-type InnerSearchResult = crate::response::Response<Vec<SearchResult>>;
+pub(crate) type InnerSearchResult = crate::response::Response<Vec<SearchResult>>;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SearchResult {
